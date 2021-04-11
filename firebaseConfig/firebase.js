4790832ce78162ec24d5b5a40457c924890e56e2
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2021-04-11 16:37:49 +0200

    Update firebase.js

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PRODJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SERVER_ID,
-  appId: process.env.APP_ID,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PRODJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SERVER_ID,
+  appId: APP_ID,
 };
 
 const app = !firebase.apps.length

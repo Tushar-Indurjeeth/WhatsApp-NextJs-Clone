53bd7326c8dfe639556be44c7d89858a9dbd2b4c
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2021-04-11 16:05:36 +0200

    Update firebase.js

--- firebaseConfig/firebase.js
+++ firebaseConfig/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtBHT9U7OZ2KkHUJnIxzEVxAGmqy4QwTA",
-  authDomain: "whatsapp-nextjs-clone.firebaseapp.com",
-  projectId: "whatsapp-nextjs-clone",
-  storageBucket: "whatsapp-nextjs-clone.appspot.com",
-  messagingSenderId: "321759526115",
-  appId: "1:321759526115:web:73e2351276fbb2073759d6",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = !firebase.apps.length

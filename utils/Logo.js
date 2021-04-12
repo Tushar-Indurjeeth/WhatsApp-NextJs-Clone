95dbc3c31fd285fca4af99129a6c462bd6e02cf8
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2021-04-12 13:16:34 +0200

    Update Logo.js

--- utils/Logo.js
+++ utils/Logo.js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Logo() {
-  return <Image src="https://res.cloudinary.com/dthcbsgjy/image/upload/v1618226036/WhatsappNextJsClone/logo_enpua0.png" alt="Logo" width="200" height="200" />;
+  return <img src="https://res.cloudinary.com/dthcbsgjy/image/upload/v1618226036/WhatsappNextJsClone/logo_enpua0.png" alt="Logo" width="200" height="200" />;
 }
